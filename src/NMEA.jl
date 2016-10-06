7b13e9734c3a17bf58e97e9e7ce01820466b2248
brandonkmiller <brandonkentmiller@gmail.com>
2016-10-06 16:31:07 -0400

    fix to hopefully fix travis ci build

--- src/NMEA.jl
+++ src/NMEA.jl
@@ -382,7 +382,7 @@ end # function parse_msg!
 #----------
 # determines system from message type string
 #----------
-function get_system(mtype::SubString{String})
+function get_system(mtype::Any)
     system = ""
 
     # GPS
